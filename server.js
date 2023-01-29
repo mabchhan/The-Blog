652aeacd883dd44dccc458a46c0d0dc9d90b7289
Mab <mabchhan@gmail.com>
2023-01-28 18:40:35 -0800

    setup server.js

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log("Now listening"));
+
+const sess = {
+  secret: "super super secret",
+  cookie: { MaxAge: 60 * 60 * 1000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// set Handlebars as the default template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
