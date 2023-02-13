6480c1876fe0ea551179453ee51283818fe02d57
Mab <mabchhan@gmail.com>
2023-02-13 10:39:18 -0800

    updated login and signup to work

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
 const routes = require("./controllers");
+const path = require("path");
 // handlebars
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static(path.join(__dirname, "public")));
 const sess = {
   secret: "super super secret",
-  cookie: { MaxAge: 60 * 60 * 1000 },
+  cookie: { MaxAge: 10 * 1000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -22,7 +23,6 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(
@@ -37,3 +38,9 @@ sequelize.sync({ force: false }).then(() => {
     )
   );
 });
+
+// app.listen(PORT, () =>
+//   console.log(
+//     `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+//   )
+// );
